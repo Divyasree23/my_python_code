fa12482a7a11d0c8f299accdbb6d6bff6a4b4915
Divya sree Neela <neeladivyasree@gmail.com>
2024-06-04 19:01:28 +0100

    adding Day 4 work

---
+++ everydaytask/day4.py
@@ -0,0 +1,10 @@
+str=["egg","swim","walk","run","jump","dance"]
+#output:- ['dance', 'jump', 'run','swim','walk']
+n = len(str)
+
+for i in range(n):
+    for j in range(n-i-1):
+        print(n,i,j,str[j],str[j+1],str)
+        if str[j] > str[j+1]:
+            str[j],str[j+1]=str[j+1],str[j]
+print(str)
