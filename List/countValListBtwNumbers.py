4eb4769119203f0ac50c7ca01223a4f21e180caf
Divyasree23 <57261013+Divyasree23@users.noreply.github.com>
2024-06-08 20:57:42 +0100

    Update countValListBtwNumbers.py

--- List/countValListBtwNumbers.py
+++ List/countValListBtwNumbers.py
@@ -1,11 +1,10 @@
-num=[10,20,30,40,50,40,40,60,70]
-start = 30
-end = 70
-
+num=[10,20,30,40,50,40,40,88,60,70]
+start = num.index(30)
+end = num.index(70)
 def func(num,start,end):
     count_1 = 0
-    for i in num:
-        if start <= i <= end:
+    for i in range(len(num)):
+        if start < i < end:
             count_1 += 1
     return count_1
 
