1ab1aba8c9f2ff717bfce25b078e30ef9bc6af61
Divyasree23 <57261013+Divyasree23@users.noreply.github.com>
2024-06-11 19:09:10 +0100

    Update day9.py

--- everydaytask/day9.py
+++ everydaytask/day9.py
@@ -1,3 +1,4 @@
+#Problem 1:
 input_list = [1,2,3,4,5,6,7,8,9,10]   # Example output: [4, 5, 1, 2, 3]
 k=12
 def rotate_array(arr, k):
@@ -13,3 +14,14 @@ print(result)  # Output: [4, 5, 1, 2, 3]
 
 #output
 #[9, 10, 1, 2, 3, 4, 5, 6, 7, 8]
+
+#Problem 2:
+#find the starting indices of all occurrences of the pattern in the string o/t [0,1]
+str = "abracardabaacdefabr"
+substr = "abr"
+lst = []
+for i in range(0,len(str)):
+    if str[i:i+len(substr)] == substr:
+        lst.append(i)
+print(lst)
+#Output : [0, 7, 16]
