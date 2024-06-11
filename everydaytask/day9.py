76b89b2e6d9ca204b2ec696b63d53297f908196a
Divyasree23 <57261013+Divyasree23@users.noreply.github.com>
2024-06-11 18:57:57 +0100

    Update day9.py

--- everydaytask/day9.py
+++ everydaytask/day9.py
@@ -4,9 +4,12 @@ def rotate_array(arr, k):
     n = len(arr)
     k = k % n
     #print(k)
-    print(arr[-k:],arr[:-k])
+    #print(arr[-k:],arr[:-k])
     return arr[-k:] + arr[:-k]
 
 # Example usage:
 result = rotate_array(input_list, k)
 print(result)  # Output: [4, 5, 1, 2, 3]
+
+#output
+#[9, 10, 1, 2, 3, 4, 5, 6, 7, 8]
