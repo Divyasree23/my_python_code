08938d1005c98b8ab62c3645a48451fc2a825118
Divyasree23 <57261013+Divyasree23@users.noreply.github.com>
2024-07-01 17:22:02 +0100

    Create day23.py

---
+++ everydaytask/day23.py
@@ -0,0 +1,37 @@
+#four program
+num = 4
+for i in range(1,num+1):
+    for j in range(1,num+1):
+        print(j,end=" ")
+    print()
+print()
+
+#five program
+for i in range(1,num+1):
+    for j in range(1,num+1):
+        print(i,end=" ")
+    print()
+print()
+
+#six program
+for i in range(1,num+1):
+    for j in range(1,num+1):
+        print('*',end=" ")
+    print()
+
+#Output:
+1 2 3 4 
+1 2 3 4 
+1 2 3 4 
+1 2 3 4 
+
+1 1 1 1 
+2 2 2 2 
+3 3 3 3 
+4 4 4 4 
+
+* * * * 
+* * * * 
+* * * * 
+* * * * 
+
